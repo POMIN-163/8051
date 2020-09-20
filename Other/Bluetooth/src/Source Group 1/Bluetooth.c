7b8f27eac02409032be4ff9dc895631a08bcd9a2
POMIN-163 <1637766030@qq.com>
2020-09-20 10:01:30 +0800

    2020-9-20

--- Other/Bluetooth/src/Source Group 1/Bluetooth.c
+++ Other/Bluetooth/src/Source Group 1/Bluetooth.c
@@ -1,13 +1,9 @@
 #include "REG51.H"
 #define uint unsigned int
 #define uchar unsigned char
-sbit k3 = P3 ^ 2;
-sbit k4 = P3 ^ 3;
 
-sbit led1 = P2 ^ 1;
-sbit led2 = P2 ^ 2;
-sbit led3 = P2 ^ 3;
-sbit beep = P2 ^ 5;
+sbit d1 = P2 ^ 1;
+sbit d2 = P2 ^ 2;
 
 uchar rec;
 
@@ -16,8 +12,8 @@ void PORT_START()
     TMOD = 0x20;
     PCON = 0x80;
     SCON = 0x50;
-    TH1 = 0xf3;
-    TL1 = 0xf3;
+    TH1 = 0xf9;
+    TL1 = 0xf9;
     TR1 = 1;
     ES = 1;
     EA = 1;
@@ -25,25 +21,30 @@ void PORT_START()
 
 void port() interrupt 4
 {
-    rec = SBUF;
+
     RI = 0;
+    rec = SBUF;
     SBUF = rec + 0x01;
-    while (!TI)
-        ;
+    rec -= 0x80;
+
+    while (!TI);
     TI = 0;
 }
 void main()
 {
     PORT_START();
+    rec = 0
+    ;
     while (1)
     {
-        if (rec % 2)
+        switch (rec)
         {
-            P2 = 0x00;
-        }
-        else
-        {
-            P2 = 0xff;
+            case 1:d1 = ~d1; rec = 0;break;
+            // case 2:d1 = 1; rec = 0;break;
+            // case 3:led1 = ~led1;rec = 0;break;
+            // case 4:led1 = ~led1;rec = 0;break;
+            default:
+                break;
         }
     }
 }

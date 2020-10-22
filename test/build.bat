027f59fca48c5b273aa8bdad7b88750f4fda10b3
POMIN-163 <1637766030@QQ.com>
2020-10-22 15:03:00 +0800

    2020-10-22

--- test/build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-c:\Users\16377\.vscode\extensions\cl.eide-1.13.0\bin\ARM\General_Builder_V6.exe -b D:\Maker\keil\C51 -d d:\My\keilmake\C51\test\.EIDE -M c:\Users\16377\.vscode\extensions\cl.eide-1.13.0\bin\ARM\8051.keil.model.json -p d:\My\keilmake\C51\test\out\Keil_C51\builder.params -o d:\My\keilmake\C51\test\out\Keil_C51 -m Normal
